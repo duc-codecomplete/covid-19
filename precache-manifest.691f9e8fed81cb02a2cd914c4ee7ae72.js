f804611d63c7cb94a3e1bece320ea7a921d5c143
duc.nguyendo <duc.nguyen@codecomplete.jp>
2020-03-27 16:57:41 +0700

    Updates

---
+++ precache-manifest.691f9e8fed81cb02a2cd914c4ee7ae72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9de3ac2d6bd217c2bcf44dc3209883f",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "28e59172d9cc915b9b80",
+    "url": "/covid-19/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0232c22f1a5ace9f5079",
+    "url": "/covid-19/static/css/main.c5f76932.chunk.css"
+  },
+  {
+    "revision": "28e59172d9cc915b9b80",
+    "url": "/covid-19/static/js/2.e8fcd607.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/covid-19/static/js/2.e8fcd607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0232c22f1a5ace9f5079",
+    "url": "/covid-19/static/js/main.1a0dc326.chunk.js"
+  },
+  {
+    "revision": "98e53ea13fc27e12979d",
+    "url": "/covid-19/static/js/runtime-main.7086b18a.js"
+  }
+]);
